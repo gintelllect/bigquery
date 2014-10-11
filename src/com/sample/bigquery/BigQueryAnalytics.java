900ab32b8142c3181b00ff4ad85c65f04726cff0
Girish Kulkarni <girishku@cybage.com>
2014-10-11 09:57:09 +0530

    Added function to Disable SSL Check if needed

--- src/com/sample/bigquery/BigQueryAnalytics.java
+++ src/com/sample/bigquery/BigQueryAnalytics.java
@@ -5,10 +5,19 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
@@ -117,5 +126,34 @@ public class BigQueryAnalytics {
 			}
 		}
 	}
+	
+	public static void disableCertificateValidation() {
+    // Create a trust manager that does not validate certificate chains
+    TrustManager[] trustAllCerts = new TrustManager[] {new X509TrustManager() {
+      public X509Certificate[] getAcceptedIssuers() {
+        return new X509Certificate[0];
+      }
+
+      public void checkClientTrusted(X509Certificate[] certs, String authType) {}
+
+      public void checkServerTrusted(X509Certificate[] certs, String authType) {}
+    }};
+
+    // Ignore differences between given hostname and certificate hostname
+    HostnameVerifier hv = new HostnameVerifier() {
+      public boolean verify(String hostname, SSLSession session) {
+        return true;
+      }
+    };
+
+    // Install the all-trusting trust manager
+    try {
+      SSLContext sc = SSLContext.getInstance("SSL");
+      sc.init(null, trustAllCerts, new SecureRandom());
+      HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+      HttpsURLConnection.setDefaultHostnameVerifier(hv);
+    } catch (Exception e) {
+    }
+  }
 
 }
